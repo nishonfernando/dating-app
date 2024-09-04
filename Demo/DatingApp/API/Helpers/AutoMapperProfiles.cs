839cb30f1a9606bb6f67c5565393abe6c6e81218
nishonfernando <nishonfernando@gmail.com>
2024-09-04 16:07:05 +1000

    API - update user profile changes added

--- Demo/DatingApp/API/Helpers/AutoMapperProfiles.cs
+++ Demo/DatingApp/API/Helpers/AutoMapperProfiles.cs
@@ -15,5 +15,6 @@ public class AutoMapperProfiles : Profile
             )
             .ForMember(d => d.Age, o => o.MapFrom(s => s.DateOfBirth.CalculateAge()));
         CreateMap<Photo, PhotoDto>();
+        CreateMap<MemberUpdateDto,AppUser>();
     }
 }
