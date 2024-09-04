bbf15de00113e836a61c9209aa917571fad8737e
nishonfernando <nishonfernando@gmail.com>
2024-09-04 15:28:42 +1000

    UI - Adding update functionality with twoway binding

--- Demo/DatingApp/client/src/app/members/member-edit/member-edit.component.ts
+++ Demo/DatingApp/client/src/app/members/member-edit/member-edit.component.ts
@@ -1,9 +1,10 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
 import { Member } from '../../_models/member';
 import { AccountService } from '../../_services/account.service';
 import { MembersService } from '../../_services/members.service';
 import { TabsModule } from 'ngx-bootstrap/tabs';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-member-edit',
@@ -15,7 +16,9 @@ import { FormsModule } from '@angular/forms';
 export class MemberEditComponent implements OnInit {
   private accountService = inject(AccountService);
   private memberService = inject(MembersService);
+  private toastr = inject(ToastrService);
   member?: Member;
+  @ViewChild('editForm') editForm?: NgForm;
 
   ngOnInit(): void {
     this.loadMember();
@@ -27,5 +30,11 @@ export class MemberEditComponent implements OnInit {
     this.memberService.getMember(user.username).subscribe({
       next: member => this.member = member
     });
+  }
+
+  updateMember(){
+    console.log(this.member);
+    this.toastr.success('Profile updated successfully');
+    this.editForm?.reset(this.member);
   }
 }
