e753a06b2a982134f311ceeb7072b756383a1799
nishonfernando <nishonfernando@gmail.com>
2024-09-05 23:10:27 +1000

    API - add cloudinary photo upload services

--- Demo/DatingApp/API/Extensions/ApplicationServiceExtensions.cs
+++ Demo/DatingApp/API/Extensions/ApplicationServiceExtensions.cs
@@ -1,4 +1,5 @@
 using API.Data;
+using API.Helpers;
 using API.Interfaces;
 using API.Services;
 using Microsoft.EntityFrameworkCore;
@@ -16,8 +17,10 @@ public static class ApplicationServiceExtensions
         });
         services.AddCors();
         services.AddScoped<ITokenService, TokenService>();
+        services.AddScoped<IPhotoService, PhotoService>();
         services.AddScoped<IUserRepository, UserRepository>();
         services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        services.Configure<CloudinarySettings>(configuration.GetSection("CloudinarySettings"));
 
         return services;
     }
