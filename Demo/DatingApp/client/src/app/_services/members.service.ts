55cae95b91af252d1222915c4ddfa73acac33618
nishonfernando <nishonfernando@gmail.com>
2024-09-12 10:35:46 +1000

    UI - getting single user from Member cache(Map)

--- Demo/DatingApp/client/src/app/_services/members.service.ts
+++ Demo/DatingApp/client/src/app/_services/members.service.ts
@@ -54,9 +54,11 @@ export class MembersService {
   }
 
   getMember(username: string) {
-    // const member = this.members().find(x => x.username === username);
+    const member: Member = [...this.memberCache.values()]
+      .reduce((arr, elem) => arr.concat(elem.body), [])
+      .find((m: Member) => m.username === username);
 
-    // if (member !== undefined) return of(member);
+    if (member) return of(member);
 
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
