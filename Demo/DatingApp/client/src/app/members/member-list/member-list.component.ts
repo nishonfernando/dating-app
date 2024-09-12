496d3b99f2ad1deea2dfb78e0dc3398c7615f174
nishonfernando <nishonfernando@gmail.com>
2024-09-12 10:47:06 +1000

    UI - remembering the filters for member when you come back from another component

--- Demo/DatingApp/client/src/app/members/member-list/member-list.component.ts
+++ Demo/DatingApp/client/src/app/members/member-list/member-list.component.ts
@@ -15,9 +15,7 @@ import { ButtonsModule } from 'ngx-bootstrap/buttons';
   styleUrl: './member-list.component.css'
 })
 export class MemberListComponent implements OnInit {
-  private accountService = inject(AccountService);
   memberService = inject(MembersService);
-  userParams = new UserParams(this.accountService.currentUser());
   genderList = [{ value: 'male', display: 'Males' }, { value: 'female', display: 'Females' }];
 
   ngOnInit(): void {
@@ -25,17 +23,17 @@ export class MemberListComponent implements OnInit {
   }
 
   loadMembers() {
-    this.memberService.getMembers(this.userParams);
+    this.memberService.getMembers();
   }
 
   resetFilters() {
-    this.userParams = new UserParams(this.accountService.currentUser());
+    this.memberService.resetUserParams();
     this.loadMembers();
   }
 
   pageChanged(event: any) {
-    if (this.userParams.pageNumber !== event.page) {
-      this.userParams.pageNumber = event.page;
+    if (this.memberService.userParams().pageNumber !== event.page) {
+      this.memberService.userParams().pageNumber = event.page;
       this.loadMembers();
     }
   }
