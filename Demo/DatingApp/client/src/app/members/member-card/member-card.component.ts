c3dab54d877a4e13daa3cc6286fe09190e440492
nishonfernando <nishonfernando@gmail.com>
2024-09-15 23:13:18 +1000

    UI - toggle like for user

--- Demo/DatingApp/client/src/app/members/member-card/member-card.component.ts
+++ Demo/DatingApp/client/src/app/members/member-card/member-card.component.ts
@@ -15,4 +15,16 @@ export class MemberCardComponent {
   member = input.required<Member>();
   //hasLiked will become a computed signal.
   hasLiked = computed(() => this.likeService.likeIds().includes(this.member().id));
+
+  toggleLike() {
+    this.likeService.togglelike(this.member().id).subscribe({
+      next: () => {
+        if (this.hasLiked()) {
+          this.likeService.likeIds.update(ids => ids.filter(x => x !== this.member().id));
+        } else {
+          this.likeService.likeIds.update(ids => [...ids, this.member().id]);
+        }
+      }
+    });
+  }
 }
