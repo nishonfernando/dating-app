bd900655df810836dcd749d3b60014f9e582579b
nishonfernando <nishonfernando@gmail.com>
2024-09-16 15:11:04 +1000

    API - getting the messages

--- Demo/DatingApp/API/Controllers/MessagesController.cs
+++ Demo/DatingApp/API/Controllers/MessagesController.cs
@@ -1,6 +1,7 @@
 using API.DTOs;
 using API.Entities;
 using API.Extensions;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
@@ -38,5 +39,17 @@ public class MessagesController(IMessageRepository messageRepository, IUserRepos
             return Ok(mapper.Map<MessageDto>(message));
 
         return BadRequest("Could not save message!");
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessagesForUser([FromQuery] MessageParams messageParams)
+    {
+        messageParams.Username = User.GetUsername();
+
+        var messages = await messageRepository.GetMessagesForUser(messageParams);
+
+        Response.AddPaginationHeader(messages);
+
+        return messages;
     }
 }
