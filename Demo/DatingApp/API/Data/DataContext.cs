9e6862205c7234afe2879eebb9ee9490479a5905
nishonfernando <nishonfernando@gmail.com>
2024-09-16 12:22:19 +1000

    API - New Entity for Messages

--- Demo/DatingApp/API/Data/DataContext.cs
+++ Demo/DatingApp/API/Data/DataContext.cs
@@ -7,6 +7,7 @@ public class DataContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<AppUser> Users { get; set; }
     public DbSet<UserLike> Likes { get; set; }
+    public DbSet<Message> Messages { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
@@ -28,5 +29,15 @@ public class DataContext(DbContextOptions options) : DbContext(options)
             //SQL Server might give an error for this(on delete cascade, if so you'll have to use NoAction); 
             //SQL lite and other databases might be ok
             .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Entity<Message>()
+            .HasOne(x => x.Recipient)
+            .WithMany(x => x.MessagesReceived)
+            .OnDelete(DeleteBehavior.Restrict);
+            
+        builder.Entity<Message>()
+            .HasOne(x => x.Sender)
+            .WithMany(x => x.MessagesSent)
+            .OnDelete(DeleteBehavior.Restrict);
     }
 }
--- Demo/DatingApp/API/Entities/AppUser.cs
+++ Demo/DatingApp/API/Entities/AppUser.cs
@@ -21,6 +21,8 @@ public class AppUser
     public List<Photo> Photos { get; set; } = [];
     public List<UserLike> LikedUsers { get; set; } = [];
     public List<UserLike> LikedByUsers { get; set; } = [];
+    public List<Message> MessagesSent { get; set; } = [];
+    public List<Message> MessagesReceived { get; set; } = [];
 
     //If you have this logic here, the DB query will always get the full object from the table and select what's needed.
     //If you move this to the automapper configuration, the DB query will only take what's requested
