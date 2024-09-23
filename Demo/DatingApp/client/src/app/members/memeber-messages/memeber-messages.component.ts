bac99a392eaf0caed542ccd991e394491e56a5b7
nishonfernando <nishonfernando@gmail.com>
2024-09-23 23:25:46 +1000

    UI - sending messages

--- Demo/DatingApp/client/src/app/members/memeber-messages/memeber-messages.component.ts
+++ Demo/DatingApp/client/src/app/members/memeber-messages/memeber-messages.component.ts
@@ -1,20 +1,30 @@
-import { Component, inject, input, OnInit } from '@angular/core';
+import { Component, inject, input, output, ViewChild } from '@angular/core';
 import { Message } from '../../_models/message';
 import { MessageService } from '../../_services/message.service';
 import { TimeagoModule } from 'ngx-timeago';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-memeber-messages',
   standalone: true,
-  imports: [TimeagoModule],
+  imports: [TimeagoModule, FormsModule],
   templateUrl: './memeber-messages.component.html',
   styleUrl: './memeber-messages.component.css'
 })
-export class MemeberMessagesComponent implements OnInit {
+export class MemeberMessagesComponent {
+  @ViewChild('messageForm') messageForm?: NgForm;
   private messageService = inject(MessageService);
   username = input.required<string>();
   messages = input.required<Message[]>();
+  messageContent = '';
+  updateMessages = output<Message>();
 
-  ngOnInit(): void {
+  sendMessage() {
+    this.messageService.sendMessage(this.username(), this.messageContent).subscribe({
+      next: message => {
+        this.updateMessages.emit(message);
+        this.messageForm?.reset();
+      }
+    });
   }
 }
