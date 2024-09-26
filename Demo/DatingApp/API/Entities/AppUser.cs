a2c86c81d1d052e98c4f42483962998bdbc624df
nishonfernando <nishonfernando@gmail.com>
2024-09-27 09:11:56 +1000

    API - entities added for the identity management

--- Demo/DatingApp/API/Entities/AppUser.cs
+++ Demo/DatingApp/API/Entities/AppUser.cs
@@ -1,13 +1,9 @@
-using API.Extensions;
+using Microsoft.AspNetCore.Identity;
 
 namespace API.Entities;
 
-public class AppUser
+public class AppUser : IdentityUser<int>
 {
-    public int Id { get; set; }
-    public required string UserName { get; set; }
-    public byte[] PasswordHash { get; set; } = [];
-    public byte[] PasswordSalt { get; set; } = [];
     public DateOnly DateOfBirth { get; set; }
     public required string KnownAs { get; set; }
     public DateTime Created { get; set; } = DateTime.UtcNow;
@@ -23,6 +19,7 @@ public class AppUser
     public List<UserLike> LikedByUsers { get; set; } = [];
     public List<Message> MessagesSent { get; set; } = [];
     public List<Message> MessagesReceived { get; set; } = [];
+    public ICollection<AppUserRole> UserRoles { get; set; } = [];
 
     //If you have this logic here, the DB query will always get the full object from the table and select what's needed.
     //If you move this to the automapper configuration, the DB query will only take what's requested
