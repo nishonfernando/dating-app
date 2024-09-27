30e11fa83c9ebf804aefd2ec3ea465b57e0a2615
nishonfernando <nishonfernando@gmail.com>
2024-09-27 15:02:46 +1000

    UI - added admin component

--- Demo/DatingApp/client/src/app/app.routes.ts
+++ Demo/DatingApp/client/src/app/app.routes.ts
@@ -11,6 +11,7 @@ import { TestErrorsComponent } from './errors/test-errors/test-errors.component'
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { preventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
 import { memberDetailedResolver } from './_resolvers/member-detailed.resolver';
+import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -24,6 +25,7 @@ export const routes: Routes = [
             { path: 'member/edit', component: MemberEditComponent, canDeactivate: [preventUnsavedChangesGuard] },
             { path: 'lists', component: ListsComponent },
             { path: 'messages', component: MessagesComponent },
+            { path: 'admin', component: AdminPanelComponent },
         ]
     },
     { path: 'errors', component: TestErrorsComponent },
