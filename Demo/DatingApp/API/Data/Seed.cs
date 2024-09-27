098d813b001f8d93ab0d5f399b91fd3fbb94bebe
nishonfernando <nishonfernando@gmail.com>
2024-09-27 11:45:58 +1000

    API - seeding users with lowercase username

--- Demo/DatingApp/API/Data/Seed.cs
+++ Demo/DatingApp/API/Data/Seed.cs
@@ -21,6 +21,7 @@ public class Seed
 
         foreach (var user in users)
         {
+            user.UserName = user.UserName!.ToLower();
             await userManager.CreateAsync(user, "Pa$$w0rd");
         }
     }
